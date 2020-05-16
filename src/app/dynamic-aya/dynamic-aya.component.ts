e5eb61978e31c93c35b444531a638a6b1a3237c6
selnggar <selnggar@ntgclarity.com>
2020-05-16 21:27:00 +0200

    bind motashabeh to aya component

--- src/app/dynamic-aya/dynamic-aya.component.ts
+++ src/app/dynamic-aya/dynamic-aya.component.ts
@@ -5,10 +5,13 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
   styleUrls: ['./dynamic-aya.component.scss']
 })
 export class DynamicAyaComponent implements OnInit {
+  @Input() allMotashabehat: any[];
   @Input() ayaNumber: number;
   @Input() href: any;
   @Input() top: any;
   @Input() left: any;
+  @Input() start: any;
+  @Input() end: any;
   @Input() activeAya: any;
   @Input() spans: any[];
   @Output() onClick: EventEmitter<any> = new EventEmitter<any>();
@@ -23,5 +26,13 @@ export class DynamicAyaComponent implements OnInit {
     this.activeAya = ayaNum;
     // event.preventDefault();
     // event.stopPropagation();
+  }
+  bakgroundStyle= {background:"white",opacity: 0.0,motashOpacity:1};
+  onMouseEnter($event) {
+    this.bakgroundStyle = {background:"yellow",opacity: .2,motashOpacity:0.2};
+  }
+
+  onMouseOut($event: MouseEvent) {
+    this.bakgroundStyle = {background:"white",opacity: 0.0,motashOpacity:1};
   }
 }
--- src/app/holy-quran/holy-quran.component.ts
+++ src/app/holy-quran/holy-quran.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-holy-quran',
@@ -6,11 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./holy-quran.component.scss']
 })
 export class HolyQuranComponent implements OnInit {
+  @Input() allMotashabehat: any[];
+  a=[];
+  childs=[];
 
   constructor() { }
 
   ngOnInit() {
+    debugger;
+    // this.allMotashabehat.forEach(arr=>{
+    //   console.log('allMotashabehat: '+arr);
+    // });
+
+
+    this.allMotashabehat.forEach(mot=>{
+      this.a.push(mot[mot.length-1].motashabeh[mot[mot.length-1].motashabeh.length-1].arrOfWords);
+      mot[mot.length-1].motashabeh[mot[mot.length-1].motashabeh.length-1].arrOfWords.forEach(word=>{
+      if(word.motashabeh.length>0&&word.motashabeh.length<15){
+      if(word.motashabeh.length>7){
+        let suras=[];
+        word.motashabeh.forEach(modeh=>{
+          if(suras.indexOf(modeh.sura)<0){
+            suras.push(modeh.sura);
+          }
+        });
+        if(suras.length<=6){
+          word.motashabeh.forEach(modeh=>{
+              this.childs.push({ayaNum:mot[mot.length-1].ayaNum,sura:modeh.suraWithIndex,char:word.nOfChar*10,word:word.word});
+
+          });
+        }
+        // break;
+        // this.childs.push({sura:mot[mot.length-1].motashabeh[0].sura,char:word.nOfChar*10,word:word.word});
+
+      } else if(word.motashabeh.length>1){
+        word.motashabeh.forEach(modeh=>{
+          if(modeh.index != word.ayaNum){
+            this.childs.push({ayaNum:mot[mot.length-1].ayaNum,sura:modeh.suraWithIndex,char:word.nOfChar*10,word:word.word});
+          }
+
+
+        });// break;
+      }
+      }
+      });
+    });
+    console.log(this.a);
+    console.log(this.childs);
+    // console.log(this.a.length);
+
   }
-inputs = [{top: '150px',href:'#17',activeAya:'17',spans:[{top:'50px',left: '45px',width: '392px',height: '35px'},{top:'90px',left: '100px',width: '337px',height: '35px'}]},{top: '550px',href:'#18',activeAya:'18',spans:[{top:'90px',left: '45px',width: '40px',height: '35px'},{top:'130px',left: '265px',width: '172px',height: '35px'}]}];
+inputs = [{start:'50',end:'130',top: '150px',href:'#17',activeAya:'17',spans:[{top:'50px',left: '45px',width: '392px',height: '35px'},{top:'90px',left: '100px',width: '337px',height: '35px'}]},
+          {start:'130',end:'170',top: '550px',href:'#18',activeAya:'18',spans:[{top:'90px',left: '45px',width: '40px',height: '35px'},{top:'130px',left: '265px',width: '172px',height: '35px'}]},
+          {start:'130',end:'170',top: '550px',href:'#18',activeAya:'18',spans:[{top:'90px',left: '45px',width: '40px',height: '35px'},{top:'130px',left: '265px',width: '172px',height: '35px'}]},
+          ];
 
 }
