22809d35126bb8acca9bae14993cff8111ca77be
romaysaa.husien <you@example.com>
2021-08-21 17:18:25 +0200

    add repititon to audios

--- src/app/listen-setting/listen-setting.component.ts
+++ src/app/listen-setting/listen-setting.component.ts
@@ -44,6 +44,7 @@ export class ListenSettingComponent implements OnInit {
   texts: string[];
   results: string[];
   currentIndex: number;
+  repeated: boolean;
 
   constructor(private router: Router,private http: HttpClient) {
   }
@@ -141,115 +142,68 @@ export class ListenSettingComponent implements OnInit {
   roow:any;
   audioCount:number;
 
-
+sorats:any[]=[];
   fromSoraFun($event: any) {
     debugger
-    this.parts=[];
-    // this.audio = 'http://cdn.alquran.cloud/media/audio/surah/' + "ar.alafasy"+ '/' + this.ayaId + '';
+    this.sorats=[];
     let url ="http://api.alquran.cloud/v1/surah/"+$event.value.nOFSura;
-    // http://api.alquran.cloud/v1/juz/2
     this.http.get<any>(url  ).subscribe(res => {
       this.audioCount = res.data.ayahs.length;
       res.data.ayahs.forEach((aya)=>{
         this.roow='http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ aya.number;
-        this.parts.push(this.roow);
+        this.sorats.push(this.roow);
         console.log(res);
       });
     });
-
-  }
-
-
-  toSoraFun($event: any) {
-    this.toSora = $event.value.nOFSura;
-
-    if (this.toSora) {
-      this.parts = [];
-      this.hezb = [];
-      this.pages = [];
-      this.rob = [];
-      this.toSoraAyat = [];
+    this.fromSora = $event.value.nOFSura;
 
 
-      this.parts.push({الجزء: '.'});
-      this.hezb.push({nOFHezb: '.'});
-      this.rob.push({rub: '.'});
-      this.pages.push({nOFPage: '.'});
-      this.toSoraAyat.push({id: '.'});
-
-      this._search.table_othmani.forEach(aya => {
-
-        if (parseInt(aya.nOFSura) <= parseInt(this.toSora) && parseInt(aya.nOFSura) >= parseInt(this.fromSora)) {
-          let index = this.parts.findIndex(sura => {
-            return aya.nOFJoz == sura.الجزء;
-          });
-
-          if (index < 0) {
-            this.parts.push({
-              الجزء: aya.nOFJoz,
-
-            });
-          }
-
-          index = this.hezb.findIndex(sura => {
-            return aya.nOFHezb == sura.nOFHezb;
-          });
-
-          if (index < 0) {
-            this.hezb.push({
-              nOFHezb: aya.nOFHezb,
-
-            });
-          }
-
-          index = this.pages.findIndex(sura => {
-            return aya.nOFPage == sura.nOFPage;
-          });
-
-          if (index < 0) {
-            this.pages.push({
-              nOFPage: aya.nOFPage,
-
-            });
-          }
-
-          index = this.rob.findIndex(sura => {
-            return aya.rub == sura.rub;
-          });
-
-          if (index < 0) {
-            this.rob.push({
-              rub: aya.rub,
-              ayaId: aya.id,
-            });
-          }
-
-        }
-
-      });
-    }
+    this.fromSoraAyat = [];
+    this.fromSoraAyat.push({id: '.'});
     let nOfAyas = $event.value.nOfAyas;
     let index = 0;
     while (index < nOfAyas) {
       index++;
-      this.toSoraAyat.push({
+      this.fromSoraAyat.push({
         id: index,
       });
     }
-    // this.result.push(this.toSora);
 
   }
 
   fromAyaFun($event: any) {
+    debugger
     this.fromAya = $event.value.id;
   }
-
+ayat:any[]=[]
   toAyaFun($event: any) {
+    debugger
+    this.sorats=[]
+    this.ayat=[]
+
     this.toAya = $event.value.id;
-  }
+    for(this.fromAya;this.fromAya<this.toAya;this.fromAya++) {
+      this.ayat.push(this.fromAya);
+    }
+    this.audioCount=this.ayat.length;
+    this.ayat.forEach((aya)=>{
+      this.roow='http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ aya;
+      this.sorats.push(this.roow);
+    });
+    }
 
   fromRobFun($event: any) {
     this.fromRob = $event.value.ayaId;
+    let url ="https://api.quran.com/api/v4/quran/verses/uthmani_simple?rub_number="+ $event.value.ayaId;
+    this.http.get<any>(url  ).subscribe(res => {
+      this.audioCount = res.verses.length;
+      res.verses.forEach((aya)=>{
+        this.roow='http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ aya.id;
+        this.sorats.push(this.roow);
+        console.log(res);
+      });
+    });
+
   }
 
   toRobFun($event: any) {
@@ -259,6 +213,16 @@ export class ListenSettingComponent implements OnInit {
 
   fromHezpFun($event: any) {
     this.fromHezp = $event.value.nOFHezb;
+    let url ="https://api.quran.com/api/v4/quran/verses/uthmani_simple?hizb_number="+ $event.value.nOFHezb;
+
+    this.http.get<any>(url  ).subscribe(res => {
+      this.audioCount = res.verses.length;
+      res.verses.forEach((aya)=>{
+        this.roow='http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ aya.id;
+        this.sorats.push(this.roow);
+        console.log(res);
+      });
+    });
   }
 
   toHezpFun($event: any) {
@@ -283,7 +247,19 @@ export class ListenSettingComponent implements OnInit {
   }
 
   fromPageFun($event: any) {
+    debugger
     this.fromPage = $event.value.nOFPage;
+    this.fromPart = $event.value.الجزء;
+    let url ="http://api.quran.com/api/v4/quran/verses/uthmani_simple?page_number="+ $event.value.nOFPage;
+
+    this.http.get<any>(url  ).subscribe(res => {
+      this.audioCount = res.verses.length;
+      res.verses.forEach((aya)=>{
+        this.roow='http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ aya.id;
+        this.sorats.push(this.roow);
+        console.log(res);
+      });
+    });
   }
 
   toPageFun($event: any) {
@@ -292,72 +268,56 @@ export class ListenSettingComponent implements OnInit {
 
   fromPartFun($event: any) {
     this.fromPart = $event.value.الجزء;
+    let url ="http://api.alquran.cloud/v1/juz/"+ $event.value.الجزء;
+      this.http.get<any>(url  ).subscribe(res => {
+      this.audioCount = res.data.ayahs.length;
+      res.data.ayahs.forEach((aya)=>{
+        this.roow='http://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/'+ aya.number;
+        this.sorats.push(this.roow);
+        console.log(res);
+      });
+    });
+
   }
 
   toPartFun($event: any) {
     this.toPart = $event.value.الجزء;
   }
-  // https://alquran.cloud/surah/2
-  // https://alquran.cloud/ayah
-  // https://alquran.cloud/juz/1
+
   audio: any;
-  saveSearch() {
-    this.result.push(
-      {
-        'fromSora': this.fromSora, 'toSora': this.toSora,
-        'fromPart': this.fromPart, 'toPart': this.toPart,
-        'fromHezp': this.fromHezp, 'toHezp': this.toHezp,
-        'fromRob': this.fromRob, 'toRob': this.toRob,
-        'fromPage': this.fromPage, 'toPage': this.toPage,
-        'fromAya': this.fromAya, 'toAya': this.toAya,
-        'omomQuaanBoolean': this.omomQuraan,
-      });
 
-    localStorage.setItem('result', JSON.stringify(this.result));
-    localStorage.setItem('dynamic_cols', JSON.stringify(this.texts));
-
-    this.router.navigate(['/navbar']);
-    alert('تم حفظ الاعدادات ');
-  }
-
+numberOfRepeated:number;
   wasatClicked($event: MouseEvent) {
-    this.omomQuraan = false;
-  }
-
-  search1(event) {
-    this.results = [];
-    this.results.push('رقم_السورة');
-    this.results.push('بداية_السورة');
-    this.results.push('الربع');
-    this.results.push('الجزء');
-    this.results.push('رقم_الجزء');
-    this.results.push('الحزب');
-    this.results.push('رقم_الحزب');
-    this.results.push('رقم_الصفحة');
-    this.results.push('بداية_الربع');
-    this.results.push('بداية_الصفحة');
-    this.results.push('اسم_السورة');
-    this.results.push('الآية');
+    this.repeated = true;
+    this.numberOfRepeated=3;
   }
 
   onChange($event: any) {
 
     this.texts.push($event);
   }
-
+played:boolean;
   audioEnded(ayaNum){
+    debugger
     if(ayaNum<this.audioCount-1){
       this.currentIndex = ayaNum+1;
       this.playNextAya(this.currentIndex );
     }
+    if(ayaNum==this.audioCount-1&&this.repeated){
+      let audio:any = document.getElementById("surahPlayer0");
+      audio.play();
+    }
   }
 
+
   playNextAya(ayaNum){
+    debugger
     let currentAudio:any = document.getElementById("surahPlayer"+ayaNum);
     currentAudio.play();
   }
 
   onPlay(ayaNum: number) {
+    debugger
     if(ayaNum != this.currentIndex+1){// handle manual play
       for(let i=0;i<this.audioCount;i++){
         if(i!=ayaNum){
